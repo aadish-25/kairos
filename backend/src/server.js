b2d44ed7bc4eb61f38d56577496932dd64377c7d
Aadish Sonawane <aadishpcs@gmail.com>
2026-02-09 23:35:54 +0530

    minor fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,5 +6,5 @@ dotenv.config({ path: "./.env" });
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT} at http://localhost:${PORT}`);
 });
