c5623500f2a85aa21a11728a8e5c534c59fa0f43
Aadish Sonawane <aadishpcs@gmail.com>
2026-02-14 20:00:40 +0530

    added morgan for terminal debugging

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import morgan from "morgan";
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: "16kb" }));
